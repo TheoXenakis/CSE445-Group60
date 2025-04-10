8fe213ccee1acaa137165029ff494675fad3001c
JohnBostater <jbostate@asu.edu>
2025-04-10 10:54:46 -0700

    Edits to project & file names to correlate to Assn #5 Rubric

---
+++ PublicPages/PublicPages/Default.aspx.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+/*
+[Authors]:
+
+    John Bostater
+
+    Theo Xenakis
+
+    Roen Wainscoat
+
+[Creation Date]: 4/9/25
+
+[Description]:
+  Action Event Handling for the Default or 'Main' page of the Web Application
+
+  User's are able to navigate to the other web pages from here..
+*/
+
+
+namespace PublicPages
+{
+    public partial class Default : System.Web.UI.Page
+    {
+      //Action-Event Handling
+      //-------------------------------------------------------------
+
+        //Executes upon Page loading
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        //[Button]: Member Page
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+        //[Button]: Staff Page
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+      //-------------------------------------------------------------
+    }
+}
