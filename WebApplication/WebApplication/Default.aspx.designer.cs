e3cac601dbc366f7b21e6f3f1b12efb3807cf968
JohnBostater <jbostate@asu.edu>
2025-04-20 14:50:15 -0700

    Added Entry to table to fit the new format

--- WebApplication/WebApplication/Default.aspx.designer.cs
+++ WebApplication/WebApplication/Default.aspx.designer.cs
@@ -51,24 +51,6 @@ namespace WebApplication
         protected global::System.Web.UI.WebControls.Button Button2;
 
         /// <summary>
-        /// Label3 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Label Label3;
-
-        /// <summary>
-        /// LinkButton1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.LinkButton LinkButton1;
-
-        /// <summary>
         /// LinkButton3 control.
         /// </summary>
         /// <remarks>
