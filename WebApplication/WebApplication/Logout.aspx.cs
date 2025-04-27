cf9ed0c1b0b1c025f40f81965891659dfd8a2832
Roen Wainscoat <13713430+roenw@users.noreply.github.com>
2025-04-26 18:12:33 -0700

    logout.aspx

---
+++ WebApplication/WebApplication/Logout.aspx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication
+{
+    public partial class Logout : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            ExpireCookie("memberLoggedIn");
+            ExpireCookie("accountUsername");
+            ExpireCookie("staffLoggedIn");
+            ExpireCookie("accountType");
+
+            Session.Clear();
+            Session.Abandon();
+
+            Response.Redirect("Default.aspx");
+        }
+
+        private void ExpireCookie(string cookieName)
+        {
+            if (Request.Cookies[cookieName] != null)
+            {
+                HttpCookie cookie = new HttpCookie(cookieName);
+                cookie.Expires = DateTime.Now.AddDays(-1); // Set expiration to the past
+                Response.Cookies.Add(cookie);
+            }
+        }
+    }
+}
